e6715eba17f2ebdea7fb6f1ff1ad86e5ec0e378e
dilpreet <dilpreetk000@gmail.com>
2017-11-08 13:57:44 -0800

    update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDk1sUgiLFSXBq1LFX-QQzg9DCSRuIGhuM",
+    authDomain: "asq-app-9ba9b.firebaseapp.com",
+    databaseURL: "https://asq-app-9ba9b.firebaseio.com",
+    projectId: "asq-app-9ba9b",
+    storageBucket: "asq-app-9ba9b.appspot.com",
+    messagingSenderId: "718451838306"
+  }
+};
