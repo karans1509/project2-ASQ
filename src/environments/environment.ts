da65800fd3106208af7c7475a20d65a76a8d3108
dilpreet <dilpreetk000@gmail.com>
2017-11-09 22:40:15 -0800

    first

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,5 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyDk1sUgiLFSXBq1LFX-QQzg9DCSRuIGhuM",
-    authDomain: "asq-app-9ba9b.firebaseapp.com",
-    databaseURL: "https://asq-app-9ba9b.firebaseio.com",
-    projectId: "asq-app-9ba9b",
-    storageBucket: "asq-app-9ba9b.appspot.com",
-    messagingSenderId: "718451838306"
-  }
+  
 };
