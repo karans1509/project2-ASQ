b3668545845f4ffeda8d9e76a1585f4b14f56d6d
karans1509 <karans1509@gmail.com>
2017-11-09 22:54:59 -0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,28 +3,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-<<<<<<< HEAD
   production: false,
-  firebase: {
-    apiKey: "AIzaSyDk1sUgiLFSXBq1LFX-QQzg9DCSRuIGhuM",
-    authDomain: "asq-app-9ba9b.firebaseapp.com",
-    databaseURL: "https://asq-app-9ba9b.firebaseio.com",
-    projectId: "asq-app-9ba9b",
-    storageBucket: "asq-app-9ba9b.appspot.com",
-    messagingSenderId: "718451838306"
-  }
-||||||| merged common ancestors
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyDk1sUgiLFSXBq1LFX-QQzg9DCSRuIGhuM",
-    authDomain: "asq-app-9ba9b.firebaseapp.com",
-    databaseURL: "https://asq-app-9ba9b.firebaseio.com",
-    projectId: "asq-app-9ba9b",
-    storageBucket: "asq-app-9ba9b.appspot.com",
-    messagingSenderId: "718451838306"
-  }
-=======
-  production: false,
-  
->>>>>>> da65800fd3106208af7c7475a20d65a76a8d3108
 };
